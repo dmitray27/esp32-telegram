14d2267f50497b7b03b1f4c08a0c725e9cab77ca
dmitray27 <dmitrayketz@gmail.com>
2025-02-19 13:59:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-n,nmnmnm
+from flask import Flask
+   app = Flask(__name__)
+
+   @app.route('/')
+   def hello():
+       return "Привет, Termux!"
+
+   if __name__ == '__main__':
+       app.run(host='0.0.0.0', port=5000)
