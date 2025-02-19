4b90061c0a350033850eba75dcd25a155f378900
dmitray27 <dmitrayketz@gmail.com>
2025-02-19 19:40:46 +0300

    Add new file

--- hello.py
+++ hello.py
@@ -1,12 +1,11 @@
 from flask import Flask
-
-# Создаём экземпляр приложения Flask
+                                                        # Создаём экземпляр приложения Flask
 app = Flask(__name__)
 
 # Определяем маршрут для главной страницы
 @app.route('/')
 def hello_world():
-    return 'Привет, мир!'
+    return 'Привет, мир!helllllo'
 
 # Запускаем приложение, если файл запущен напрямую
 if __name__ == '__main__':
